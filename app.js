58755b77ef1df345b18d047c6eb1b6d223c5d141
chalusb <chaluuf@gamil.com>
2026-02-19 17:51:59 -0600

    Allow PATCH in CORS

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use((err,req,res,next) => {
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
 
